d9c5e4213409039dea80b16c73d54f4ecfbeb3da
Karan Evgeniy <63317606+karanzhenya@users.noreply.github.com>
2023-03-03 09:56:58 +0300

    refactoring

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import logo from '../assets/images/letter.png';
 
 function Navbar() {
     return (
         <div className="navbar">
             <div className="navbar-container">
-                <img src="logo" alt="logo"/>
+                <img src="../assets/images/letter.png" alt="logo"/>
                 <ul>
                     <li><Link to={"/"}>Home</Link></li>
                     <li><Link to={"/tasks"}>Tasks</Link></li>
