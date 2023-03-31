1df4b15e19d98f9d42e17eacf0ec1902fcd85414
Karan Evgeniy <63317606+karanzhenya@users.noreply.github.com>
2023-03-31 14:36:43 +0300

    set width img

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ function Home() {
     return (
         <div className="home">
             <div className="home-container">Home Page</div>
-            <img src={"https://static.db.motorland.by/part/7593821/0.jpg"} alt={"caliper"}/>
+            <img src={"https://static.db.motorland.by/part/7593821/0.jpg"} alt={"caliper"} style={{width: "70%"}}/>
         </div>
     );
 }
