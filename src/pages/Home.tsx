ef604ee06dd89b29d1cd988ed90d96d2dbd55b65
Karan Evgeniy <63317606+karanzhenya@users.noreply.github.com>
2023-03-31 14:25:07 +0300

    fix img tag

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ function Home() {
     return (
         <div className="home">
             <div className="home-container">Home Page</div>
-            <image src={"https://static.db.motorland.by/part/7593821/0.jpg"} alt={"caliper"}/>
+            <img src={"https://static.db.motorland.by/part/7593821/0.jpg"} alt={"caliper"}/>
         </div>
     );
 }
