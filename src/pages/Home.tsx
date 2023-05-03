a6aa24388afb7520e06979af3cbb5264322a450d
Karan Evgeniy <63317606+karanzhenya@users.noreply.github.com>
2023-05-03 10:21:29 +0300

    fix url

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ function Home() {
         <div className="home">
             <div className="home-container">
                 <span>Home Page</span>
-                <img src={"https://www.dropbox.com/s/09pvpi704jatrjc/calendar.png?dl=0"} alt={"caliper"} style={{width: "70%"}}/>
+                <img src={"https://previews.dropbox.com/p/thumb/AB632L59VE_qa9Y6KIoXh38jf3_csrmGTsTji8AT6yTkp2KNaJf2lo6Asf78ecUFcuQ-jE3RiiD5MF20zDDBIHxuXcFjovBxecJyX9M5u5849XeIJWyHZfb_PJUGEu0FUAO-29Jlnu65lDYxnaTOT1q0hbX80-n3ZnCQYikVMG0hW_n-N0bsqiAVX5EIrqXymNoTalYViBvmd8dxnNcVrjJFmK6mvT5F8MVzw06tF3QgFeYfGaSMnn_I6TNcaVPQB7Q7FaUb_fosDnfpQ8wN4CIYoxh21gRHtGIC0oYYU6a2Jc8upytk3WpiN8un8LeoAXDVzWeV1S_cuAb4A4cWb3wt8bAKti-xLSiZqtZrIPmxtwg9iI6jNn9t7jxxKbhbnSY/p.png"} alt={"caliper"} style={{width: "70%"}}/>
             </div>
         </div>
     );
