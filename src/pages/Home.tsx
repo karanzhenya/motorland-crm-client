70fd7a938e4a2d6c82c7058ddf23525e97465dd2
Karan Evgeniy <63317606+karanzhenya@users.noreply.github.com>
2023-05-03 09:08:38 +0300

    add calendar image to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ function Home() {
         <div className="home">
             <div className="home-container">
                 <span>Home Page</span>
-                <img src={"https://static.db.motorland.by/part/7593821/0.jpg"} alt={"caliper"} style={{width: "70%"}}/>
+                <img src={"https://4.downloader.disk.yandex.ru/preview/af1468c6551e9651cb3791faaa09ecb69a32903163655b2538eb6cda4764ac55/inf/9asrvxLCb7hD8cyJ8A5pAVE2jLW_-rsjqf_t-8snDmFB8ZNwQXh67blrBhMwnr22Xgq1NNxHKmlRuCchMkJIWQ%3D%3D?uid=220232774&filename=calendar.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=220232774&tknv=v2&size=1903x969"} alt={"caliper"} style={{width: "70%"}}/>
             </div>
         </div>
     );
