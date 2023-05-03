95d632d72d21c41f17e83f0715e74925ce9e9231
Karan Evgeniy <63317606+karanzhenya@users.noreply.github.com>
2023-05-03 10:53:10 +0300

    refactoring width image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ function Home() {
         <div className="home">
             <div className="home-container">
                 <span>Home Page</span>
-                <img src={"https://protrud.by/upload/medialibrary/230/2.jpg"} alt={"caliper"} style={{width: "70%"}}/>
+                <img src={"https://protrud.by/upload/medialibrary/230/2.jpg"} alt={"caliper"} style={{width: "50%"}}/>
             </div>
         </div>
     );
