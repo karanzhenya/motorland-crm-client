9950e6205fb2cdfc399b926e148af76cfa8f5052
karanzhenya <63317606+karanzhenya@users.noreply.github.com>
2023-05-15 00:33:50 +0300

    add car specs data, refactor fetch data

--- src/pages/AddCarInfo.tsx
+++ src/pages/AddCarInfo.tsx
@@ -1,15 +1,17 @@
 import React, {useState} from 'react';
 import axios from "axios";
 import {car_data} from "../assets/car_data";
+import {car_specs_data} from "../assets/car_specs_data";
 
 function AddCarInfo() {
     const [modelType, setModelType] = useState<string>('')
     const [info, setInfo] = useState<string>('')
     const [name, setName] = useState<string>('')
     const create = () => {
-        let changedCar = car_data.find(c => c.name.toLowerCase() == name.toLowerCase().trim())
-        if (changedCar) {
-            let obj = {modelType, info, _car_id: changedCar.id}
+        let addedCar = car_data.find(c => c.name.toLowerCase() == name.toLowerCase().trim())
+        let addedCarSpecs = car_specs_data.find(c => c.type.toLowerCase() == modelType.toLowerCase().trim())
+        if (addedCar && addedCarSpecs) {
+            let obj = {modelType, info: addedCarSpecs.info, _car_id: addedCar.id}
             axios.post('https://cars-express-back.vercel.app/carspecs', {...obj}).then((res) => {
                 console.log(res.data)
                 setModelType('')
